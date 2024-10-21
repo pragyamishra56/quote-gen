e9f2dee5d9a7d74937fe549668f17b681b85d3bc
Pragya <mi.pragya16595@gmail.com>
2024-10-21 13:25:05 +0530

    Using All Origins as a proxy

--- index.js
+++ index.js
@@ -23,8 +23,9 @@ function removeLoadingSpinner() {
 async function getQuote() {
   showLoadingSpinner()
   // const proxyUrl = 'https://cors-anywhere.herokuapp.com/'
-  // const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
-  const apiUrl = 'https://api.quotable.io/random';
+  const proxyUrl = 'https://api.allorigins.win/get?url='; // Using All Origins as a proxy
+  const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
+  // const apiUrl = 'https://api.quotable.io/random';
   try {
     const response = await fetch(proxyUrl + apiUrl);
     const data = await response.json();
