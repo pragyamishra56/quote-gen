fde4c2bd978374d3ee216d8d03830512956f4389
Pragya <mi.pragya16595@gmail.com>
2024-10-21 13:21:27 +0530

    proxy url changed

--- index.js
+++ index.js
@@ -22,8 +22,9 @@ function removeLoadingSpinner() {
 // Get Quote From API
 async function getQuote() {
   showLoadingSpinner()
-  const proxyUrl = 'https://cors-anywhere.herokuapp.com/'
-  const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
+  // const proxyUrl = 'https://cors-anywhere.herokuapp.com/'
+  // const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
+  const apiUrl = 'https://api.quotable.io/random';
   try {
     const response = await fetch(proxyUrl + apiUrl);
     const data = await response.json();
